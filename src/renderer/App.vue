68105b2bf67a65c123e32ab642828544ac521165
Ronan <rsf.sho@gmail.com>
2018-09-30 16:02:45 -0700

    screen shots

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,20 +1,62 @@
 <template>
   <div id="app">
-    <landing-page></landing-page>
+    <nav>
+      <h1>Zenith Studios Showcase</h1>
+      <button>IMPORT</button>
+    </nav>
+    <form @submit.prevent="importFilm">
+      <input v-model="path" type="text" placeholder="file path">
+      <input v-model="name" type="text" placeholder="Film Name">
+      <input v-model="productionTime" type="text" placeholder="production time">
+      <input v-model="yearMade" type="text" placeholder="year made">
+      <input v-model="screenShotTime" type="text" placeholder="timecode in seconds for screen shot">
+      <button type="submit">submit</button>
+    </form>
   </div>
 </template>
 
 <script>
-  import LandingPage from '@/components/LandingPage'
-
+  import './assets/main.scss'
+  import '../../node_modules/minireset.css/minireset.sass'
+  var ffmpeg = require('fluent-ffmpeg');
+  const fs = require('fs');
   export default {
     name: 'showcase',
-    components: {
-      LandingPage
+    data() {
+
+      return {
+        path: '',
+        name: '',
+        productionTime: '',
+        yearMade: '',
+        screenShotTime: '',
+        data: {}
+      }
+    },
+    mounted() {
+
+// First I want to read the file
+      this.data = JSON.parse(fs.readFileSync('data.json', 'utf8'));
+    },
+
+    methods: {
+      importFilm() {
+        let name = this.name + '.png'
+        console.log(name)
+        let proc = new ffmpeg(this.path)
+        .takeScreenshots({
+          count: 1,
+          timemarks: [this.screenShotTime],
+          filename: name,
+          folder: './shots/'
+        }, function (err) {
+          console.log('screenshots were saved')
+        });
+        console.log(proc)
+
+
+      }
+
     }
   }
 </script>
-
-<style>
-  /* CSS */
-</style>
